2bee2db67af4babc5adf080f8b12f413abb83153
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-07-08 09:36:36 +0530

    added dev script, a home route server response, and items

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/", (req, res) => {
+  res.json("Server is working.");
+});
+
 app.get("/items", async (req, res) => {
   const storedItems = await getStoredItems();
   // await new Promise((resolve, reject) => setTimeout(() => resolve(), 4000));
@@ -38,4 +42,7 @@ app.post("/items", async (req, res) => {
   res.status(201).json({ message: "Stored new item.", item: newItem });
 });
 
-app.listen(8080);
+app.listen(8080, () => {
+  console.log("Server has started at port 8080")
+  console.log("Click here to : http://localhost:8080");
+});
